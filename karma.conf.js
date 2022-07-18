c0343ff66762a3692c934990f85ff779e7a90c4d
Chris Stone <contact@chrisstone.dev>
2022-07-18 08:19:31 -0400

    Fix up test scripts, clean up angular.json, remove unnecessary dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -12,7 +11,6 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-teamcity-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
