b82fd329431d31b6fff8484b2851f5a06fcd7220
Chris Stone <contact@chrisstone.dev>
2022-07-20 21:00:40 -0400

    Improve style and semantics of Jamstack cards

--- src/app/why/work.component.ts
+++ src/app/why/work.component.ts
@@ -4,6 +4,7 @@ import {Title} from '@angular/platform-browser';
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
+  styleUrls: ['./work.component.scss']
 })
 export class WorkComponent implements OnInit {
   constructor(private titleService: Title) {
