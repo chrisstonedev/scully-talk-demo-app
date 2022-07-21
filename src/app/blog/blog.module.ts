3a2b6d2692a550bd70d123af86e1a32b4e01aeaf
Chris Stone <contact@chrisstone.dev>
2022-07-21 13:48:31 -0400

    Run ng g c blog/blog-list

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,11 +1,13 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {ScullyLibModule} from '@scullyio/ng-lib';
-import {BlogRoutingModule} from './blog-routing.module';
-import {BlogComponent} from './blog.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { BlogRoutingModule } from './blog-routing.module';
+import { BlogComponent } from './blog.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
-  declarations: [BlogComponent],
-  imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
+  declarations: [BlogComponent, BlogListComponent],
+  imports: [CommonModule, BlogRoutingModule, ScullyLibModule]
 })
-export class BlogModule {}
+export class BlogModule {
+}
