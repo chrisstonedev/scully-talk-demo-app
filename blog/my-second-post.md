ee8c6df9b4715edadf8c0ad2e73316d8be969adc
Chris Stone <contact@chrisstone.dev>
2022-07-21 22:46:38 -0400

    Add 2nd blog post and improve styles slightly

--- blog/my-second-post.md
+++ blog/my-second-post.md
@@ -1,7 +1,10 @@
 ---
 title: my-second-post
-description: blog description
-published: false
+description: 'blog description'
+published: true
+authorName: Chris Stone
 ---
 
 # my-second-post
+
+I've written a second post! Can you believe it?
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -4,9 +4,10 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
+import { BackComponent } from './back/back.component';
 
 @NgModule({
-  declarations: [BlogComponent, BlogListComponent],
+  declarations: [BlogComponent, BlogListComponent, BackComponent],
   imports: [CommonModule, BlogRoutingModule, ScullyLibModule]
 })
 export class BlogModule {
